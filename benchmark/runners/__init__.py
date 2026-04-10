0111b478c09a8786293d10a18935ca5219b0c8ca
xiaoran Mac Studio M1 Max <xiaoran2495897824@163.com>
2026-04-10 17:42:38 -0400

    fix: update runners/__init__.py import after find_optimal_batch_size moved to calibration

--- benchmark/runners/__init__.py
+++ benchmark/runners/__init__.py
@@ -1,4 +1,5 @@
 from benchmark.runners.base import BenchRunner, BenchResult
 from benchmark.runners.single_runner import SingleRunner
 from benchmark.runners.ddp_runner import DDPRunner
-from benchmark.runners.common import train_step, find_optimal_batch_size
+from benchmark.runners.common import train_step
+from benchmark.calibration import find_optimal_batch_size
